f1a41e2bf1984376340949357064a8e31b1a2873
Paul Griffin <paul.griffin@svt.se>
2021-02-18 14:38:16 +0100

    Remove tests

--- parkering/Package.swift
+++ parkering/Package.swift
@@ -28,9 +28,9 @@ let package = Package(
             ]
         ),
         .target(name: "Run", dependencies: [.target(name: "App")]),
-        .testTarget(name: "AppTests", dependencies: [
-            .target(name: "App"),
-            .product(name: "XCTVapor", package: "vapor"),
-        ])
+//        .testTarget(name: "AppTests", dependencies: [
+//            .target(name: "App"),
+//            .product(name: "XCTVapor", package: "vapor"),
+//        ])
     ]
 )
